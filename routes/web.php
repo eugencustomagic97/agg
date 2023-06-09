165d91701518955b089eefe746f4da1e1d9f9367
Eugen Customagic <tisagiy415@soremap.com>
2023-06-09 13:54:34 +0300

    -Development commit

--- routes/web.php
+++ routes/web.php
@@ -16,37 +16,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/test', function(){
-
-    $ip = file_get_contents('https://api.ipify.org');
-
-    dd($ip);
-    $apiUrl = \Config::get('api.api_base_url');
-    $apiUser = \Config::get('api.api_user');
-    $apiPassword = \Config::get('api.api_password');
-
-    $auth = base64_encode($apiUser . ":" . $apiPassword);
-    try {}
-    catch(Exception $e){
-        dump($e->getCode(), $e->getMessage());
-    }
-    $ch = curl_init();
-
-    curl_setopt($ch, CURLOPT_HEADER, 0);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    curl_setopt($ch, CURLOPT_URL, $apiUrl . "Agents/GetAgents/");
-    curl_setopt($ch, CURLOPT_POST, 1);
-    curl_setopt($ch, CURLOPT_USERPWD, $auth);
-
-    $data = curl_exec($ch);
-
-    if ($data === false) {
-        throw new Exception(curl_error($ch), curl_errno($ch));
-    }
-
-    curl_close($ch);
-
-
-    dump($data);
-});
